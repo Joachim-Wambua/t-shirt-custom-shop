3b4e0652719d882e42972e11c324089c4f3526e8
Joachim Mwengi <kimwambua96@gmail.com>
2023-12-16 17:35:29 +0300

    added camera and shadow settings

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -13,19 +13,44 @@ const Shirt = () => {
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
-  return (
-    <group>
-        <mesh
-            castShadow
-            geometry={nodes.T_Shirt_male.geometry}
-            material={materials.lambert1}
-            material-roughness={1}
-            dispose={null}
-        >
+  useFrame((state, delta) =>
+    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
+  );
 
-        </mesh>
-    </group>  
-  )
+  const stateString = JSON.stringify(snap);
+
+  return (
+    <group key={stateString}>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      >
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+
+        {snap.isLogoTexture && (
+          <Decal
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            // map-anisotropy={16}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
+      </mesh>
+    </group>
+  );
 };
 
 export default Shirt;
