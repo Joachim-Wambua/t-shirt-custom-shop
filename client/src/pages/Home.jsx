757827e19e0982126f0ba64f5cd624d4cd001465
Joachim Mwengi <kimwambua96@gmail.com>
2023-12-16 16:05:02 +0300

    started threeJS canvas & camera setup

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 
 import state from "../store";
+import { CustomButton } from "../components";
 
 import {
   headContainerAnimation,
@@ -26,15 +27,29 @@ const Home = () => {
 
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
-                <h1 className="head-text" >CUSTOMIZE <br className="xl:block hidden" /> COOL T-SHIRTS</h1>
+              <h1 className="head-text">
+                CUSTOMIZE <br className="xl:block hidden" /> COOL T-SHIRTS
+              </h1>
             </motion.div>
-            <motion.div className="flex flex-col gap-5" {...headContentAnimation} >
-                <p className="max-w-md font-normal text-gray-600 text-base" >
-                    Customize your exclusive unique t-shirts with our brand new customization tool. <strong>Unleash Your Creativity!</strong>{" "} and create your own style!
-                </p>
+            <motion.div
+              className="flex flex-col gap-5"
+              {...headContentAnimation}
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Customize your exclusive unique t-shirts with our brand new
+                customization tool. <strong>Unleash Your Creativity!</strong>{" "}
+                and create your own style!
+              </p>
+
+              <CustomButton 
+                type="filled"
+                title="Customize It"
+                handleClick = {() => state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+
             </motion.div>
           </motion.div>
-
         </motion.section>
       )}
     </AnimatePresence>
